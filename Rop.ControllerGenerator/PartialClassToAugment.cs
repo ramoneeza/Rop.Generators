dd9b8cbf45357fcac478971d65797c3a6b9aaf3f
Ramon <ramon@eeza.csic.es>
2023-07-10 08:38:02 +0200

    Aviod not init controller

--- Rop.ControllerGenerator/PartialClassToAugment.cs
+++ Rop.ControllerGenerator/PartialClassToAugment.cs
@@ -10,9 +10,9 @@ namespace Rop.ControllerGenerator
         public PartialClassToAugment(ClassDeclarationSyntax classToAugment) : base(classToAugment)
         {
         }
-        public IEnumerable<string> GetHeader(IEnumerable<string> additionalusings,IEnumerable<string> controllerproperties)
+        public IEnumerable<string> GetHeader(IEnumerable<string> additionalusings)
         {
-            return GetHeader0().Concat(GetHeader1()).Concat(GetNamespace0()).Concat(Attributes1(controllerproperties)).Concat(GetClass0());
+            return GetHeader0().Concat(GetHeader1()).Concat(GetNamespace0()).Concat(GetClass0());
             // Local functions
             IEnumerable<string> GetHeader1()
             {
@@ -23,9 +23,6 @@ namespace Rop.ControllerGenerator
             }
         }
 
-        public IEnumerable<string> Attributes1(IEnumerable<string> controllerproperties)
-        {
-            return controllerproperties.Select(p => $"\t\t[MemberNotNull(nameof({p}))]");
-        }
+      
     }
 }
